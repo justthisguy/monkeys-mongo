a03c1ecb1eec8cdf758d647f94fe000535ac3493
Ken Decanio <ken-decanio@41monkeys.com>
2012-05-10 20:41:14 -0700

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monkeys::Application.config.secret_token = '865e8cfa44489209d054c9c69bfd46bbe75bbf753ae4eb9a656708766e6f7c5c5296e7d32580f1904cd0c3a899f6f94ea0d2b954156afa34118e94ec6245772e'
